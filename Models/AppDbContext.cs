06c2f8d1088d557d7d8ddfe5416c56d01c3c7d17
alain <alain@NIGHTWALKER>
2022-02-21 22:30:15 -0800

    Add project files.

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Books_WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Books_WebAPI.Controllers
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+        public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=NIGHTWALKER\SQLEXPRESS;Initial Catalog=Book_DB;Integrated Security=True;Pooling=False");
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Books_WebAPI.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string bookTitle { get; set; }
+    }
+}
